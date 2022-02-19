456bcf688fe1c5272b946b021b25dbfb935e06d1
Janis Kirsteins <janis@janiskirsteins.org>
2022-02-19 15:14:52 +0000

    Add PaletteSetterView to GameBoardView

--- GameBoard.swift
+++ GameBoard.swift
@@ -4,23 +4,22 @@ struct GameBoardView: View {
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack {
-            Row(model: RowModel(word: "Hello", expected: "Hella", isSubmitted: true))
-            EditableRow(expected: "fuels")
+        PaletteSetterView {
+            VStack {
+                
+                EditableRow(expected: "fuels")
+                
+                Row(model: RowModel(word: "", expected: "Hella", isSubmitted: false))
+                Row(model: RowModel(word: "", expected: "Hella", isSubmitted: false))
+                Row(model: RowModel(word: "", expected: "Hella", isSubmitted: false))
+                Row(model: RowModel(word: "", expected: "Hella", isSubmitted: false))
+            }
         }
-            .environment(\.palette, 
-                          colorScheme == .dark ? DarkPalette() : DarkPalette())
     }
 }
 
 struct GameBoardView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            VStack {
-                Text("Hello")
-                GameBoardView()
-                    .navigationTitle("French")
-            }
-        }
+        GameBoardView()
     }
 }
