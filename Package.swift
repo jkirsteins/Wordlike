7705c461981876b85ccf694a71de56cd7f627fa4
Janis Kirsteins <janis@janiskirsteins.org>
2022-04-18 12:25:17 +0000

    1.0.9 with definition links

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "org.janiskirsteins.SimpleWordGame",
             teamIdentifier: "FN5YR78T7X",
-            displayVersion: "1.0.8",
-            bundleVersion: "11",
+            displayVersion: "1.0.9",
+            bundleVersion: "12",
             iconAssetName: "AppIcon",
             accentColorAssetName: "AccentColor",
             supportedDeviceFamilies: [
