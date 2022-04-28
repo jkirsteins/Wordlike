45074d7b37cf1e461f11aa1ac5018c814a5e4ec3
Janis Kirsteins <janis@janiskirsteins.org>
2022-04-28 05:32:39 +0000

    Fix: word matching improvements in Simple LV

--- GameLogic/SimplifiedLatvianWordValidator.swift
+++ GameLogic/SimplifiedLatvianWordValidator.swift
@@ -1,5 +1,13 @@
 import SwiftUI
 
+fileprivate func countDiacritics(_ word: String) -> Int {
+    let without = word.folding(options: .diacriticInsensitive, locale: Locale(identifier: "lv_LV"))
+    let withSet = Set(word)
+    let withoutSet = Set(without)
+    let common = withSet.intersection(withoutSet)
+    return word.count - common.count
+}
+
 class SimplifiedLatvianWordValidator : WordValidator
 {
     static let specialMap: Dictionary<String, String> = [
@@ -54,38 +62,39 @@ class SimplifiedLatvianWordValidator : WordValidator
         expected: String,
         model: [RowModel]?,
         reason: inout String?) -> String? {
-        guard word.count == 5 else {
-            reason = "Not enough letters"
-            return nil
-        } 
+            guard word.count == 5 else {
+                reason = "Not enough letters"
+                return nil
+            } 
             
             guard super.validateRequirements(word: word, model: model, reason: &reason) else {
                 return nil
             }
-        
-        if word.folding(options: [.caseInsensitive, .diacriticInsensitive], locale: nil) == expected.folding(options: [.caseInsensitive, .diacriticInsensitive], locale: nil) {
-            return expected.uppercased()
+            
+            if word.folding(options: [.caseInsensitive, .diacriticInsensitive], locale: nil) == expected.folding(options: [.caseInsensitive, .diacriticInsensitive], locale: nil) {
+                return expected.uppercased()
+            }
+            
+            let uppercaseExpected = expected.uppercased()
+            let simplifiedSubmit = simplify(word.uppercased())
+            
+            let candidates = guesses.filter {
+                simplify($0) == simplifiedSubmit
+            }.map {
+                ($0, countMatching(between: $0, and: uppercaseExpected), countDiacritics($0))
+            }
+            
+            guard let candidateResult = candidates.max(by: {
+                print("Comparing", $0, "with", $1)
+                return $0.1 < $1.1 || $0.2 < $0.2 
+            }).map({ $0.0 }) else {
+                reason = "Not in word list"
+                return nil
+            } 
+            
+            reason = nil
+            return candidateResult
         }
-        
-        let uppercaseExpected = expected.uppercased()
-        let simplifiedSubmit = simplify(word.uppercased())
-        
-        let candidates = guesses.filter {
-            simplify($0) == simplifiedSubmit
-        }.map {
-            ($0, countMatching(between: $0, and: uppercaseExpected))
-        }
-        
-        guard let candidateResult = candidates.max(by: {
-            $0.0 < $1.0
-        }).map({ $0.0 }) else {
-            reason = "Not in word list"
-            return nil
-        } 
-        
-        reason = nil
-        return candidateResult
-    }
     
     func countMatching(between word: String, and expected: String) -> Int {
         return word.reduce(0, {
@@ -151,14 +160,20 @@ struct Internal_LatvianWordValidator_TestPreviews: PreviewProvider {
         }
         
         VStack {
-            Text("ZINAS should be accepted")
-            Internal_LatvianWordValidator_TestView(answer: "ZVĪŅA", word: "ZINAS", okTestResult: "ZIŅĀS")
+            Text("ZINAS should be accepted as ZIŅAS")
+            Internal_LatvianWordValidator_TestView(answer: "ZVĪŅA", word: "ZINAS", okTestResult: "ZIŅAS")
         }
         
         VStack {
             Text("We should always choose the answer word, if it matches the entered word")
             Internal_LatvianWordValidator_TestView(answer: "ZVĪŅA", word: "ZVINA", okTestResult: "ZVĪŅA")
             Internal_LatvianWordValidator_TestView(answer: "ZVĪŅĀ", word: "ZVINA", okTestResult: "ZVĪŅĀ")
+        }
+        
+        VStack {
+            Text("We should always default to a word with less diacritics when possible")
+            Text(verbatim: "Diacritics in KŅADĀ: \(countDiacritics("KŅADĀ"))")
+            Internal_LatvianWordValidator_TestView(answer: "KROKA", word: "TORNI", okTestResult: "TORNI")
         }
     }
 }
