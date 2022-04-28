cec113afcceca3078e5ccab6c39db82b9489e8a5
Janis Kirsteins <janis@janiskirsteins.org>
2022-04-28 18:07:59 +0000

    Version bump to v1.0.33

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "org.janiskirsteins.SimpleWordGame",
             teamIdentifier: "FN5YR78T7X",
-            displayVersion: "1.0.32",
-            bundleVersion: "37",
+            displayVersion: "1.0.33",
+            bundleVersion: "38",
             iconAssetName: "AppIcon",
             accentColorAssetName: "AccentColor",
             supportedDeviceFamilies: [
