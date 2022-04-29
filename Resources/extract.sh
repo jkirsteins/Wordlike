af897dd4ea9e263b1a9172d0b8bd528c6f1eb21a
Janis Kirsteins <janis@janiskirsteins.org>
2022-04-29 05:38:22 +0000

    Trimming Latvian word list

--- Resources/extract.sh
+++ Resources/extract.sh
@@ -29,7 +29,46 @@ cat lv_wordlist_5.txt | grep -r '.*[asše]$' | \
 	grep -v -r '.*aš$' | \
 	grep -v -r '.*era$' | \
 	grep -v -r '.*eta$' | \
+	grep -v -r '.*īla$' | \
 	grep -v -r '.*ķa$' | \
+	grep -v -r '.*ata$' | \
+	grep -v -r '.*īda$' | \
+	grep -v -r '.*īta$' | \
+	grep -v -r '.*els$' | \
+	grep -v -r '.*oja$' | \
+	grep -v -r '.*īga$' | \
+	grep -v -r '.*rie$' | \
+	grep -v -r '.*āta$' | \
+	grep -v -r '.*ora$' | \
+	grep -v -r '.*ača$' | \
+	grep -v -r '.*lie$' | \
+	grep -v -r '.*oka$' | \
+	grep -v -r '.*oda$' | \
+	grep -v -r '.*īsa$' | \
+	grep -v -r '.*īna$' | \
+	grep -v -r '.*āla$' | \
+	grep -v -r '.*ada$' | \
+	grep -v -r '.*ēda$' | \
+	grep -v -r '.*ota$' | \
+	grep -v -r '.*īņa$' | \
+	grep -v -r '.*pūš$' | \
+	grep -v -r '.*āks$' | \
+	grep -v -r '.*rga$' | \
+	grep -v -r '.*oba$' | \
+	grep -v -r '.*ica$' | \
+	grep -v -r '.*āba$' | \
+	grep -v 'velba' | \
+	grep -v 'valba' | \
+	grep -v 'žilba' | \
+	grep -v 'šķība' | \
+	grep -v 'gārgs' | \
+	grep -v 'sirgs' | \
+	grep -v 'žirgs' | \
+	grep -v 'žigla' | \
+	grep -v 'sigla' | \
+	grep -v 'šķūņa' | \
+	grep -v 'ņirgs' | \
+	grep -v 'vraka' | \
 	egrep '^[[:lower:]]+$' \
 	> lv_A.tmp.txt
 
@@ -40,6 +79,28 @@ echo 'stera' >> lv_A.tmp.txt
 echo 'vieta' >> lv_A.tmp.txt
 echo 'lieta' >> lv_A.tmp.txt
 echo 'opera' >> lv_A.tmp.txt
+echo 'gnīda' >> lv_A.tmp.txt
+echo 'grīda' >> lv_A.tmp.txt
+echo 'švīta' >> lv_A.tmp.txt
+echo 'svīta' >> lv_A.tmp.txt
+echo 'flora' >> lv_A.tmp.txt
+echo 'spora' >> lv_A.tmp.txt
+echo 'sloka' >> lv_A.tmp.txt
+echo 'kroka' >> lv_A.tmp.txt
+echo 'akota' >> lv_A.tmp.txt
+echo 'grota' >> lv_A.tmp.txt
+echo 'kvota' >> lv_A.tmp.txt
+echo 'slota' >> lv_A.tmp.txt
+echo 'bļoda' >> lv_A.tmp.txt
+echo 'grēda' >> lv_A.tmp.txt
+echo 'grīņa' >> lv_A.tmp.txt
+echo 'zvīņa' >> lv_A.tmp.txt
+echo 'morga' >> lv_A.tmp.txt
+echo 'murga' >> lv_A.tmp.txt
+echo 'sērga' >> lv_A.tmp.txt
+echo 'ņerga' >> lv_A.tmp.txt
+echo 'ņirga' >> lv_A.tmp.txt
+echo 'žurga' >> lv_A.tmp.txt
 
 cat lv_A.tmp.txt | sort -u > lv_A.txt
 rm lv_A.tmp.txt
