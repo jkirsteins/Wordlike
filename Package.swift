999a09bb397648443a06078ac9d445033c51d617
Janis Kirsteins <janis@janiskirsteins.org>
2022-05-18 15:42:02 +0000

    Build bump to 70

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             bundleIdentifier: "org.janiskirsteins.SimpleWordGame",
             teamIdentifier: "FN5YR78T7X",
             displayVersion: "1.0.54",
-            bundleVersion: "68",
+            bundleVersion: "70",
             iconAssetName: "AppIcon",
             accentColorAssetName: "AccentColor",
             supportedDeviceFamilies: [
