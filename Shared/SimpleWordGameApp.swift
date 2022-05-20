3927126c83a594d492942c02520e14540395aef0
Jānis Kiršteins <janis@janiskirsteins.org>
2022-05-20 16:18:37 +0200

    Initial Commit

---
+++ Shared/SimpleWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWordGameApp.swift
+//  Shared
+//
+//  Created by Janis Kirsteins on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
